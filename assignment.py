9b38cc104769b9e5cf8f2a5d1c38568f51417fba
noobie-boobie <husainhusain.rr@gmail.com>
2023-03-04 12:46:56 -0600

    added some calculations

--- assignment.py
+++ assignment.py
@@ -3,9 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from shapely.geometry import LineString
 
-def calculateMudGradient(pressure_gradient, total_depth, depth):
-    return round((((pressure_gradient*total_depth)+300)/depth) , 2)
+def calculateMudGradient(pressureGradient, depth):
+    return round((((pressureGradient*depth)*1.1)/depth) , 2)
 
+def calculateCollapsePressure(mudGradient, depth):
+    return round(mudGradient*depth, 2)
+
+def calculateFormationPressure(pressure, depth):
+    return round( pressure* depth, 2)
+
+def calculateInternalPressure(formationPressure, depth, depthAtPreviousPoint, pressure):
+    internalPressure = formationPressure - (depth - depthAtPreviousPoint)*0.1
+    return round(internalPressure, 2)
 
 data = pd.read_csv('data.csv')
 
@@ -64,6 +73,7 @@ for i, p in enumerate(points):
     [x, y] = p
     if i!= 0 and i%2 == 0:
         continue
+    plt.text(x, y, char)
     print("{} - ".format(char), end="")
     print("({0:.2f}, ".format(x),end = "")
     print("{0:.2f})".format(-1*y))
@@ -71,16 +81,49 @@ for i, p in enumerate(points):
     char = chr(ord(char)+1)
 
 pressure_points.pop(0)
+# pressure_points = [
+#     [0.465,6200], [0.48, 10400], [0.57, 13900]
+# ]
 mud_gradients = []
-total_depth = -1 * depth[-1]
 
 for pressure_point in pressure_points:
-    mud_gradients.append( calculateMudGradient(pressure_point[0], total_depth, pressure_point[1]))
+    mud_gradients.append( calculateMudGradient(pressure_point[0], pressure_point[1]))
 
-print(mud_gradients)
+print("Pressure Points-", pressure_points)
+print("Mud Gradient-", mud_gradients)
+
+collapse_pressures = []
+for point in range(len(pressure_points)):
+    collapse_pressures.append(calculateCollapsePressure(mud_gradients[point], pressure_points[point][1]))
+
+print("Collapse Pressure-",collapse_pressures)
+
+formation_pressures = []
+for i in range(1, len(pressure_points)):
+    formation_pressures.append(calculateFormationPressure(pressure_points[i][0], pressure_points[i][1]))
+
+print("Formation Pressure-",formation_pressures)
+
+internal_pressures = []
+for i in range(1, len(pressure_points)):
+    internal_pressures.append(calculateInternalPressure(formation_pressures[i-1], pressure_points[i][1], pressure_points[i-1][1], pressure_points[i][0]))
+internal_pressures.append(formation_pressures[-1])
+
+print("Internal Pressure-",internal_pressures)
+
+external_pressures = []
+for i in range(len(pressure_points)):
+    external_pressures.append(calculateCollapsePressure(pressure_points[i][1], mud_gradients[i]))
+
+print("External Pressure", external_pressures)
+
+burst_casing_shoe = []
+for i in range(len(internal_pressures)):
+    burst_casing_shoe.append(round(internal_pressures[i] - external_pressures[i], 2))
+print("Burst Casing Shoe-", burst_casing_shoe)
 
 plt.xlabel('Pressure Gradient (psi/ft)')
 plt.ylabel('Depth (ft)')
 plt.title('Depth vs Pressure Gradient')
 
-plt.show()
+# plt.show()
